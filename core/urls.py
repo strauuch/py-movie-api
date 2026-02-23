180329a8fd6e1084476418da5299cbf7c93a6be8
Вадим Шевченко <grybefs@gmail.com>
2026-02-23 16:59:15 +0100

    fix: change url cinema in core/url to api/cinema/

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/cinema/", include("cinema.urls", namespace="cinema")),
+    path("api/cinema/", include("cinema.urls", namespace="cinema")),
 ]
