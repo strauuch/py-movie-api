8e362eacd97468a3aed0e026226f86d1e8919f8c
Вадим Шевченко <grybefs@gmail.com>
2026-02-23 16:05:30 +0100

    chore: install DRF2

--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "cinema",
     "rest_framework",
+    "cinema",
 ]
 
 MIDDLEWARE = [
