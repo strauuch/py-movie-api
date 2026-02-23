daf654d5563dc8c620372a3c098709fb8d4a92ab
Вадим Шевченко <grybefs@gmail.com>
2026-02-23 16:25:53 +0100

    style: black all

--- cinema/serializes.py
+++ cinema/serializes.py
@@ -12,8 +12,11 @@ class MovieSerializer(serializers.Serializer):
         return Movie.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.duration = validated_data.get('duration', instance.duration)
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get(
+            "description",
+            instance.description
+        )
+        instance.duration = validated_data.get("duration", instance.duration)
         instance.save()
         return instance
--- cinema/views.py
+++ cinema/views.py
@@ -22,6 +22,7 @@ def movie_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 @api_view(["GET", "PUT", "DELETE"])
 def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
